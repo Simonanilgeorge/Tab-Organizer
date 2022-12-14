7ea348be0dacd521971873b5d6fd9681dc71189e
Simonanilgeorge <smnanilgeorge@gmail.com>
2022-12-14 15:21:08 +0530

    moved code to deleteAndGroupTabs function

--- background.js
+++ background.js
@@ -1,37 +1,38 @@
 
 
 
-chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status == 'complete') {
-        chrome.tabs.query({}, async (tabs) => {
-            let map = new Map()
-            tabs.forEach(async (tab) => {
-                if (map.has(tab.url)) {
-                    await chrome.tabs.remove(tab.id, async () => {
-                    })
-                    await chrome.tabs.update({ url: tab.url })
-                }
-                else {
-                    map.set(tab.url, true)
-                }
-            })
-
-            tabs.sort((a, b) => {
-                if (a.url.split("//")[1] > b.url.split("//")[1]) {
-                    return 1
-                }
-                else if (a.url.split("//")[1] < b.url.split("//")[1]) {
-                    return -1
-                }
-                else {
-                    return 0
-                }
-            })
-
-            tabs.forEach((tab) => {
-                chrome.tabs.move(tab.id, { index: tabs.length - 1 });
-            })
-        });
-
+        chrome.tabs.query({}, deleteAndGroupTabs);
     }
 })
+
+
+function deleteAndGroupTabs(tabs) {
+    let map = new Map()
+    tabs.forEach((tab) => {
+        if (map.has(tab.url)) {
+            chrome.tabs.remove(tab.id, ()=>{})
+            chrome.tabs.update({ url: tab.url })
+        }
+        else {
+            map.set(tab.url, true)
+        }
+    })
+
+    tabs.sort((a, b) => {
+        if (a.url.split("//")[1] > b.url.split("//")[1]) {
+            return 1
+        }
+        else if (a.url.split("//")[1] < b.url.split("//")[1]) {
+            return -1
+        }
+        else {
+            return 0
+        }
+    })
+
+    tabs.forEach((tab) => {
+        chrome.tabs.move(tab.id, { index: tabs.length - 1 });
+    })
+}
